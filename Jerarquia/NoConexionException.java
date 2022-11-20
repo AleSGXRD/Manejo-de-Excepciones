71531a697eed1d52ff9a5c4fedf783cd4bcbb92a
DanteXRD <106454456+AleSGXRD@users.noreply.github.com>
2022-11-20 13:44:04 -0500

    Mayorclaridad

--- Jerarquia/NoConexionException.java
+++ Jerarquia/NoConexionException.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class NoConexionException extends LoginException{
+public class NoConexionException extends LoginErrorException{
 	public NoConexionException() {
 		super("Ha ocurrido un error de coneccion.");
 	}
